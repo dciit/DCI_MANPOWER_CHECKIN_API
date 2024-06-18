412409c6f63704f2b89dd93ee1fff35ee9c7d962
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-06-18 21:52:22 +0700

    test

---
+++ API_DCI_DIAGRAM_SVG/Controllers/ApsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_DCI_DIAGRAM_SVG.Controllers
+{
+    public class ApsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
